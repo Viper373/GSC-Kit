83eb9c3f5d39cfd1a7e54dc8bb3aabf83419423a
Viper373 <2020311228@bipt.edu.cn>
2024-10-12 09:09:13 +0800

    fit:优化代码结构

--- gsc_main.py
+++ gsc_main.py
@@ -24,16 +24,16 @@ cookies = {
     "__Secure-1PAPISID": "xDVrp0VxBBWeBD77/A4Ad0UoMoVf-UDcVT",
     "__Secure-3PAPISID": "xDVrp0VxBBWeBD77/A4Ad0UoMoVf-UDcVT",
     "NID": "518=AUVI1ArS52abtZdinujR5yjEdfc6Qo2368uBesI06o3MTq7hB0Q7adFme9rUtW5IIIUhywYYzUIwQB9jzIl7dsq6lMrf6dG34Z_OgZVlcQDL3j6tNBK0zD5dPIqizQzC6bYmmXzt6eHPsKjQlONyXSVG78JwfC-Kl44Z2B13OjBtmqcjDBZXCBjQ7Ghj8Ioyu8HqURmze-b6TwpWE-KDaFZ7xkyAYgJsufA7kZSyrXc89JZzJfCiZDei8ylEnc73P65qwdkPxZSyJSsPcVUzo8y6knLGyHFhBIeP6ZFIKuoRrGIEmytyO4GqdniMi5PgHSmIIgAyATR7ZXMpn23hW-ImJ6zPhmaKb2V3UrIfpmfO3BaeXQLALAeMlwBPy9zyM8Efh0-7kkBpeM5N9ye07pueP-xOqpxbZnhhlb17euYVSzu_DK46TRc6tXcnigb5HQmTenLiXXHBNN0uD63Ydhxvec9XP66BbYXINSTLG4dQuZ1QBBr14biXdoXzWyxUI_HGP9Vr7AqdP6u9ItIk6ASzJisf8nJVu4OatFTHzqCiLpVc7TOGBuX0r0T-eZdyMhCfkiBnmo0qhlLZgCVYxAiT-1bRT2_BUuTL-1byb1hBnHqPODBuAOUvP2xijIMPXrOJADzCtnm0xEWZbxzv8HZ7SUHkYYCfLjYN6Dw85odEGBrlZdO8Jr9orpj_Fr-UmLYFj5m8cWe045OPMvDJ1xAIVBhozsiDWh8rIrSWBFWjKe4q4TT0nKGwCWj0nf5-0RiHO-JD4eXBF3H9qNnrK1ts7VQciPXvqLUX5mgjvVQUaK4L-ul0OgxuPc07Kmjasz8fBOm9luKDEh6oedrsyeTf0vVV26dxTJXny5Y4WQ3BNfLEdQ-7xcNsb7s8nja2mlm6y45gzv3Xd4je0wfTktHK2uHLOIkztPCInv7bKprSqwWL2KqtT7HGZks3fRp0QE93n5FYbBjha-8byKPxSxLbM4JT51csE2o0DxvSAZChTymxvO3ZD7vqkElVHsw2jx3-FeuGg2WBWq0Z4MTN9RyeCtZHcIR-BzevUqaTA0-2zurJjUEgCAkX2YeN7E1EIib7wphgjQwMOIr_ktg",
-    "__Secure-1PSIDTS": "sidts-CjIBQlrA-KO9g3TspG5aQ5PkC1FLb_I6D3a5faoe_FREtXoXikvTt0OknBy-CZpLhN9NvBAA",
-    "__Secure-3PSIDTS": "sidts-CjIBQlrA-KO9g3TspG5aQ5PkC1FLb_I6D3a5faoe_FREtXoXikvTt0OknBy-CZpLhN9NvBAA",
-    "SIDCC": "AKEyXzU-JT1mKR2yH9CdHb9jV7vYAZXg-szQz7e2hs3JvZOQzY-B-bgVR8UJHcInu5_IMp8S8bY",
-    "__Secure-1PSIDCC": "AKEyXzW6YRt3cMOdTmbSel75lYibNpRpa_2s1O3faw3yQSgBUeNOuvjSTM1ezE8xF8JNQeHaWw",
-    "__Secure-3PSIDCC": "AKEyXzXdtHHE1SArkLuP9vS2eNSUh7v9e8RzvlpZjlnlVwUE1Q2vVy-96mglH8AqAIuK9E8f0Rk",
-    "_ga_QX2LK1FZEG": "GS1.1.1728642644.9.1.1728645904.0.0.0"
+    "__Secure-1PSIDTS": "sidts-CjIBQlrA-K1QI_ca-oc4qKI9UmGSUs9AO9b9XTCBUQtUG7qILxdTdk0hadot1Wkg7WbqehAA",
+    "__Secure-3PSIDTS": "sidts-CjIBQlrA-K1QI_ca-oc4qKI9UmGSUs9AO9b9XTCBUQtUG7qILxdTdk0hadot1Wkg7WbqehAA",
+    "SIDCC": "AKEyXzWKMjb6woM1C9p-28cc6WAHuWNDoKBxGID1S73f49ZpepGRSqoQvNG5xuKfc7QLw8tw7Y0",
+    "__Secure-1PSIDCC": "AKEyXzXCyEPKLkrghhrUkgGyJ5SfXXDvm2mGXV93aO-Qsx4JNFMKTF2p3ezwTrJvg8BW7ryUDw",
+    "__Secure-3PSIDCC": "AKEyXzU9wQ_ZKfhwEvzez91H9WcPLzPKTOFeInZ4Lo02-Au4iXEqfbt1fjWggDTRh_S44kTcpcg",
+    "_ga_QX2LK1FZEG": "GS1.1.1728694633.10.1.1728694641.0.0.0"
 }
 run_task_indexing = RunTaskIndexing(cookies)
 run_task_performance = RunTaskPerformance(cookies)
 
 if __name__ == '__main__':
-    run_task_performance.run_performance()
+    # run_task_performance.run_performance()
     run_task_indexing.run_indexing()
