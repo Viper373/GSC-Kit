f1fe12d50718ccdcd8db3d6da6ccf5e50dd27c8e
Viper373 <2020311228@bipt.edu.cn>
2024-10-12 15:28:29 +0800

    feat(`tool_utils/api_utils.py`):新增 APIUtils 工具类，实现 GSC 数据提交功能

---
+++ tool_utils/api_utils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Project   :td_gsc_scraper
+# @FileName  :api_utils.py
+# @Time      :2024/10/12 10:34
+# @Author    :Zhangjinzhao
+# @Software  :PyCharm
+
+import requests
+from tool_utils.log_utils import RichLogger
+
+rich_logger = RichLogger()
+
+
+class APIUtils:
+    def __init__(self):
+        self.headers = {
+            'Content-Type': 'application/json',
+        }
+        self.gsc_api_base_url = 'https://aiadmin.erweima.ai/api/gsc/acceptAndParseData'
+
+    @rich_logger
+    def post_gsc_data(self, json_data: str, json_type: str):
+        """
+        提供 GSC 数据。
+        :param json_data: JSON 数据。
+        :param json_type: JSON 数据类型。
+        :return: flag: bool: 是否成功提供数据。
+        """
+        json_data = {
+            'jsonData': json_data,
+            'jsonType': json_type,
+        }
+        try:
+            response = requests.post(url=self.gsc_api_base_url, headers=self.headers, json=json_data)
+            if response.status_code == 200:
+                return True
+            else:
+                rich_logger.error(f"GSC API 接口异常: {response.status_code}")
+                return False
+        except Exception as e:
+            rich_logger.exception(f"GSC API 接口异常: {e}")
+            return False
